86f23aac0cf0c5b8776c0346dc3ecf357a805a06
Aniket Kumar Lata <alata@codeaurora.org>
2019-07-11 14:20:23 -0700

    hal: Send A2DP backend config for each routed stream
    There exists a corner case where A2DP backend config is not
    sent after BE is closed and reopened leading to slimbus
    running at default rate. This happens because A2DP snd_device
    is already active, hence backend config for A2DP is not sent.
    However, audio route is disabled and enabled i.e. BE is closed
    and reopened. Thus, slimbus is configured at 8 KHz which is a
    problem for A2DP.
    Fix this by sending backend config for each stream that gets
    routed to A2DP and A2DP snd_device is enabled.
    
    Change-Id: I3edfcaaac9d0bc81f7be4090f8057f3947b1e5d0

--- hal/audio_extn/audio_extn.c
+++ hal/audio_extn/audio_extn.c
@@ -4206,6 +4206,9 @@ static a2dp_start_capture_t a2dp_start_capture;
 typedef int (*a2dp_stop_capture_t)();
 static a2dp_stop_capture_t a2dp_stop_capture;
 
+typedef bool (*a2dp_set_source_backend_cfg_t)();
+static a2dp_set_source_backend_cfg_t a2dp_set_source_backend_cfg;
+
 typedef int (*sco_start_configuration_t)();
 static sco_start_configuration_t sco_start_configuration;
 
@@ -4260,7 +4263,10 @@ int a2dp_offload_feature_init(bool is_feature_enabled)
             !(a2dp_start_capture =
                  (a2dp_start_capture_t)dlsym(a2dp_lib_handle, "a2dp_start_capture")) ||
             !(a2dp_stop_capture =
-                 (a2dp_stop_capture_t)dlsym(a2dp_lib_handle, "a2dp_stop_capture"))) {
+                 (a2dp_stop_capture_t)dlsym(a2dp_lib_handle, "a2dp_stop_capture")) ||
+            !(a2dp_set_source_backend_cfg =
+                 (a2dp_set_source_backend_cfg_t)dlsym(
+                                     a2dp_lib_handle, "a2dp_set_source_backend_cfg"))) {
             ALOGE("%s: dlsym failed", __func__);
             goto feature_disabled;
         }
@@ -4298,6 +4304,7 @@ feature_disabled:
     a2dp_source_is_suspended = NULL;
     a2dp_start_capture = NULL;
     a2dp_stop_capture = NULL;
+    a2dp_set_source_backend_cfg = NULL;
 
     ALOGW(":: %s: ---- Feature A2DP_OFFLOAD is disabled ----", __func__);
     return -ENOSYS;
@@ -4394,6 +4401,12 @@ int audio_extn_a2dp_start_capture()
 int audio_extn_a2dp_stop_capture()
 {
     return (a2dp_stop_capture ? a2dp_stop_capture() : 0);
+}
+
+bool audio_extn_a2dp_set_source_backend_cfg()
+{
+    return (a2dp_set_source_backend_cfg ?
+                a2dp_set_source_backend_cfg() : false);
 }
 
 int audio_extn_sco_start_configuration()
